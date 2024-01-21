d2c1d84c3b16443e9d1d9fa06ff997422c6043cd
Kai Hiwatari <85945740+ItsKai14@users.noreply.github.com>
2024-01-21 16:59:59 +0530

    Updating Index Page

--- Libraries/Card/cards.js
+++ Libraries/Card/cards.js
@@ -1,9 +1,9 @@
 angular.module('myApp', [])
   .controller('CardController', function($scope) {
     $scope.cards = [
-      { image: './Libraries/Card/assets/computer.png', name: 'PC Builds', description: 'Explore PC builds suggested by us that suits you well!', href: 'page1.html' },
-      { image: './Libraries/Card/assets/laptop.png', name: 'Laptops', description: 'Explore best laptops for school as well as office use.', href: 'page2.html' },
-      { image: './Libraries/Card/assets/keyboard.png', name: 'Accessories', description: 'Already have a system? NeverMind Explore Accessories for it', href: 'page3.html' },
-      { image: './Libraries/Card/assets/motherboard.png', name: 'Spare Parts', description: 'Have a plan? Costomise yourself by exploaring parts of your choice!', href: 'page3.html' },
+      { image: './Libraries/Card/assets/computer.png', name: 'PC Builds', description: 'Explore PC builds suggested by us that suits you well!', href: 'builds.html' },
+      { image: './Libraries/Card/assets/laptop.png', name: 'Laptops', description: 'Explore best laptops for school as well as office use.', href: 'laptops.html' },
+      { image: './Libraries/Card/assets/keyboard.png', name: 'Accessories', description: 'Already have a system? NeverMind Explore Accessories for it', href: 'accessories.html' },
+      { image: './Libraries/Card/assets/motherboard.png', name: 'PC Components', description: 'Have a plan? Costomise yourself by exploaring components of your choice!', href: 'components.html' },
     ];
   });
